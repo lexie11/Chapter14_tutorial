7c5b90c71b3b43f77e6343e83803997c9f43ccee
lexie11 <44230267+lexie11@users.noreply.github.com>
2019-06-16 22:09:18 +0100

    removing print functions
    removing print functions

--- scraper.py
+++ scraper.py
@@ -1,16 +1,8 @@
 import scraperwiki
 html = scraperwiki.scrape('https://inmo.ie/6022')
-print "Click on the ...more link to see the whole page"
-# print html
-
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
 tds = root.cssselect('td') # get all the <td> tags
-print tds
-for td in tds:
-    print lxml.html.tostring(td) # the full HTML tag
-    print td.text                # just the text inside the HTML tag
-
 for td in tds:
     record = { "td" : td.text } # column name and value
     try:
