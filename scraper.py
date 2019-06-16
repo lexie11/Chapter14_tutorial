92c360ff301cac25e2696a36966b206ef78683bb
lexie11 <44230267+lexie11@users.noreply.github.com>
2019-06-16 21:34:15 +0100

    comment out print html
    comment out print html

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import scraperwiki
 html = scraperwiki.scrape('https://inmo.ie/6022')
 print "Click on the ...more link to see the whole page"
-print html
+# print html
 
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
