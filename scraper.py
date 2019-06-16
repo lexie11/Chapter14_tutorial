14785196a33b1eff4c233f50d3cc9581fab7044c
lexie11 <44230267+lexie11@users.noreply.github.com>
2019-06-16 21:21:57 +0100

    parse raw html to get interesting bits
    parse raw html to get interesting bits

--- scraper.py
+++ scraper.py
@@ -2,3 +2,10 @@ import scraperwiki
 html = scraperwiki.scrape('https://inmo.ie/6022')
 print "Click on the ...more link to see the whole page"
 print html
+
+import lxml.html
+root = lxml.html.fromstring(html) # turn our HTML into an lxml object
+tds = root.cssselect('td') # get all the <td> tags
+for td in tds:
+    print lxml.html.tostring(td) # the full HTML tag
+    print td.text                # just the text inside the HTML tag
